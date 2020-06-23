3020c0edb15e585deb6cb894d3ccdc75568cbdf7
KenBauer-FSD <63252588+KenBauer-FSD@users.noreply.github.com>
2020-06-23 09:30:27 -0500

    Update ConnectionTest.psm1

--- ConnectionTest.psm1
+++ ConnectionTest.psm1
@@ -1,29 +1,31 @@
-Function ConnectionTestDown{
+Function ConnectionTest{
     param (
-        [Parameter(Mandatory=$true)]$HostName,
-        [Parameter(Mandatory=$true)]$To,
-        [Parameter(Mandatory=$true)]$From
+        [Parameter(Mandatory=$true)]
+            $HostName,
+            $To,
+            $From,
+        [Parameter(Mandatory=$false)]
+            [Switch]$Down,
+            [Switch]$Up
     )
     $cred = get-credential
     
-    DO {
-        $successconnectionStatus = Test-Connection -ComputerName $HostName -Quiet
-        Start-Sleep -seconds 5}
-        while ($successconnectionStatus -eq $True)
+    if($Down -eq $true -and $Up -eq $false){
+        DO {
+            $successconnectionStatus = Test-Connection -ComputerName $HostName -Quiet
+            Start-Sleep -seconds 5}
+            while ($successconnectionStatus -eq $True)
                 Send-MailMessage -To $To -From $From -Subject "$HostName Connection Down" -Body "The connection to $Hostname is down." -BodyAsHtml -smtpserver smtp.office365.com -usessl -Credential $cred -Port 587
-    }
-    
-    Function ConnectionTestUp{
-        param (
-            [Parameter(Mandatory=$true)]$HostName,
-            [Parameter(Mandatory=$true)]$To,
-            [Parameter(Mandatory=$true)]$From
-        )
-        $cred = get-credential
-        
+        }         
+    elseif ($Up -eq $true -AND $Down -eq $false) {
         DO {
             $failedconnectionStatus = Test-Connection -ComputerName $HostName -Quiet
             Start-Sleep -seconds 5}
         while ($failedconnectionStatus -eq $False)
         Send-MailMessage -To $To -From $From -Subject "$HostName Connection Up" -Body "The connection to $Hostname is up." -BodyAsHtml -smtpserver smtp.office365.com -usessl -Credential $cred -Port 587
     }
+    else {
+        Write-Error -Message "Error: -Down or -Up specified improperly."
+    }
+    }
+    
