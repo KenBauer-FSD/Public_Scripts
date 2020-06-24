9befe5c4fd08721b3449f6633da70c0124e6a68e
Ken Bauer <ken.bauer@fsdnet.ca>
2020-06-24 12:13:29 -0500

    Changed sleep to 5 minutes

--- ConnectionTest.psm1
+++ ConnectionTest.psm1
@@ -13,14 +13,14 @@ Function ConnectionTest{
     if($Down -eq $true -and $Up -eq $false){
         DO {
             $successconnectionStatus = Test-Connection -ComputerName $HostName -Quiet
-            Start-Sleep -seconds 5}
+            Start-Sleep -Seconds 300}
             while ($successconnectionStatus -eq $True)
                 Send-MailMessage -To $To -From $From -Subject "$HostName Connection Down" -Body "The connection to $Hostname is down." -BodyAsHtml -smtpserver smtp.office365.com -usessl -Credential $cred -Port 587
         }         
     elseif ($Up -eq $true -AND $Down -eq $false) {
         DO {
             $failedconnectionStatus = Test-Connection -ComputerName $HostName -Quiet
-            Start-Sleep -seconds 5}
+            Start-Sleep -seconds 300}
         while ($failedconnectionStatus -eq $False)
         Send-MailMessage -To $To -From $From -Subject "$HostName Connection Up" -Body "The connection to $Hostname is up." -BodyAsHtml -smtpserver smtp.office365.com -usessl -Credential $cred -Port 587
     }
